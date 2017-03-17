d5bbbf18c8e9cb019c43c469966e94128fe2a0ff
René Puls <rp@kianga.eu>
2017-03-17 11:52:27 +0100

    Fix code formatting

--- cmd/safetycheck/main.go
+++ cmd/safetycheck/main.go
@@ -11,9 +11,8 @@ import (
 	"os"
 	"strings"
 
-    jwt "github.com/dgrijalva/jwt-go"
+	jwt "github.com/dgrijalva/jwt-go"
 )
-
 
 var hostnameFlag = flag.String("hostname", "attest.android.com", "Hostname for certificate validation")
 
@@ -32,22 +31,22 @@ func loadCertificate(b64data string) (*x509.Certificate, error) {
 
 func ValidateToken(tokenStr string) (*jwt.Token, error) {
 	parsed, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
-        // Verify signing method
+		// Verify signing method
 		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
 		}
 
-        // Extract certificates from x5c header
+		// Extract certificates from x5c header
 		certStrings, ok := token.Header["x5c"].([]interface{})
-        if !ok {
-            return nil, fmt.Errorf("Missing certificates (x5c) in header")
-        }
+		if !ok {
+			return nil, fmt.Errorf("Missing certificates (x5c) in header")
+		}
 		certs := make([]*x509.Certificate, len(certStrings))
 		for i, v := range certStrings {
 			certString, ok := v.(string)
-            if !ok {
-                return nil, fmt.Errorf("Invalid type in certificate list")
-            }
+			if !ok {
+				return nil, fmt.Errorf("Invalid type in certificate list")
+			}
 			cert, err := loadCertificate(certString)
 			if err != nil {
 				return nil, fmt.Errorf("Malformed certificate at index %d: %v", i, err)
@@ -60,7 +59,7 @@ func ValidateToken(tokenStr string) (*jwt.Token, error) {
 		mainCert := certs[0]
 		chainCerts := certs[1:]
 
-        // Validate certificates
+		// Validate certificates
 		intermediatePool := x509.NewCertPool()
 		for _, cert := range chainCerts {
 			intermediatePool.AddCert(cert)
@@ -76,13 +75,13 @@ func ValidateToken(tokenStr string) (*jwt.Token, error) {
 		return mainCert.PublicKey, nil
 	})
 
-    if err != nil {
-        return nil, fmt.Errorf("Parse error: %v", err)
-    }
+	if err != nil {
+		return nil, fmt.Errorf("Parse error: %v", err)
+	}
 
-    if !parsed.Valid {
-        return nil, fmt.Errorf("Token is not valid")
-    }
+	if !parsed.Valid {
+		return nil, fmt.Errorf("Token is not valid")
+	}
 
 	if err := parsed.Claims.Valid(); err != nil {
 		return nil, fmt.Errorf("Invalid claims: %v", err)
