41b1b2b8925648b722a41314bbff7cf5ef0a0d4b
René Puls <rp@kianga.eu>
2017-03-17 11:48:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# safetycheck
+
+Command line utility for verifying (JSON Web Tokens)[http://self-issued.info/docs/draft-ietf-oauth-json-web-token.html] from Google's SafetyNet API
+
+## Installation
+
+Set `GOPATH` as necessary, then:
+
+```
+go install github.com/kianga/safetycheck/cmd/safetycheck
+```
+
+## Usage
+
+```
+$ safetycheck < good-token.txt
+{"apkCertificateDigestSha256":["K64XBz2c/rGGr8msokWAdJeevhYZztQ6S7buXfdSu4o="],"apkDigestSha256":"EC6DSwK8ODHyAc6uRpLf0QqBZkHpW7pOyJP+aVeMyv4=","apkPackageName":"com.example.package","basicIntegrity":true,"ctsProfileMatch":true,"extension":"CQHxcA1H0//M","nonce":"DMnSi/ya8fM0PLslPaHTZg==","timestampMs":1489726139356}
+(exit code 0)
+
+$ safetycheck < bad-token.txt
+2017/03/17 11:32:53 Failed to validate token: Parse error: crypto/rsa: verification error
+(exit code 1)
+```
+
+## Dependencies
+
+* (jwt-go)[https://github.com/dgrijalva/jwt-go]
---
+++ cmd/safetycheck/main.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"crypto/x509"
+	"encoding/base64"
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+    jwt "github.com/dgrijalva/jwt-go"
+)
+
+
+var hostnameFlag = flag.String("hostname", "attest.android.com", "Hostname for certificate validation")
+
+func loadCertificate(b64data string) (*x509.Certificate, error) {
+	buf := make([]byte, base64.StdEncoding.DecodedLen(len(b64data)))
+	l, err := base64.StdEncoding.Decode(buf, []byte(b64data))
+	if err != nil {
+		return nil, fmt.Errorf("Invalid base64 encoding")
+	}
+	cert, err := x509.ParseCertificate(buf[:l])
+	if err != nil {
+		return nil, fmt.Errorf("Cannot parse certificate: %v", err)
+	}
+	return cert, nil
+}
+
+func ValidateToken(tokenStr string) (*jwt.Token, error) {
+	parsed, err := jwt.Parse(tokenStr, func(token *jwt.Token) (interface{}, error) {
+        // Verify signing method
+		if _, ok := token.Method.(*jwt.SigningMethodRSA); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+
+        // Extract certificates from x5c header
+		certStrings, ok := token.Header["x5c"].([]interface{})
+        if !ok {
+            return nil, fmt.Errorf("Missing certificates (x5c) in header")
+        }
+		certs := make([]*x509.Certificate, len(certStrings))
+		for i, v := range certStrings {
+			certString, ok := v.(string)
+            if !ok {
+                return nil, fmt.Errorf("Invalid type in certificate list")
+            }
+			cert, err := loadCertificate(certString)
+			if err != nil {
+				return nil, fmt.Errorf("Malformed certificate at index %d: %v", i, err)
+			}
+			certs[i] = cert
+		}
+		if len(certs) < 1 {
+			return nil, fmt.Errorf("Empty certificate list in header")
+		}
+		mainCert := certs[0]
+		chainCerts := certs[1:]
+
+        // Validate certificates
+		intermediatePool := x509.NewCertPool()
+		for _, cert := range chainCerts {
+			intermediatePool.AddCert(cert)
+		}
+		verifyOpts := x509.VerifyOptions{
+			DNSName:       *hostnameFlag,
+			Intermediates: intermediatePool,
+		}
+		if _, err := mainCert.Verify(verifyOpts); err != nil {
+			return nil, fmt.Errorf("Invalid signing certificate: %v", err)
+		}
+
+		return mainCert.PublicKey, nil
+	})
+
+    if err != nil {
+        return nil, fmt.Errorf("Parse error: %v", err)
+    }
+
+    if !parsed.Valid {
+        return nil, fmt.Errorf("Token is not valid")
+    }
+
+	if err := parsed.Claims.Valid(); err != nil {
+		return nil, fmt.Errorf("Invalid claims: %v", err)
+	}
+
+	return parsed, err
+}
+
+func main() {
+	flag.Parse()
+
+	tokenBytes, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatalf("Error reading input: %v", err)
+	}
+
+	token, err := ValidateToken(strings.TrimSpace(string(tokenBytes)))
+	if err != nil {
+		log.Fatalf("Failed to validate token: %v", err)
+	}
+
+	encoder := json.NewEncoder(os.Stdout)
+	encoder.Encode(token.Claims)
+}
